bb1348e8886534efcd8b2d88c2b2e9d6fee11e1f
xuan <mxuanp@protonmail.com>
2020-04-13 16:33:52 +0800

    修改xfmt的打印函数

--- xfmt/xfmt.go
+++ xfmt/xfmt.go
@@ -14,7 +14,12 @@ const (
 	White
 )
 
-//彩色打印文字
+//PrintMsg 彩色打印文字
 func PrintMsg(msg string, color int) {
-	fmt.Printf("%c[%d;%d;%dm%s%c[0m\n", 0x1B, 0, 0, color, msg, 0x1B)
+	fmt.Printf("%c[%d;%d;%dm%s%c[0m", 0x1B, 0, 0, color, msg, 0x1B)
+}
+
+//PrintlnMsg 彩色打印文字，自动换行
+func PrintlnMsg(msg string, color int) {
+	PrintMsg(msg+"\n", color)
 }
